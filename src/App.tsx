7082aed5544d659c1f30cb569296692e5c265d79
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-08-12 10:18:36 +0100

    add wlist form

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import { Hero } from "./assets/components/hero";
 import { Navbar } from "./assets/components/navbar";
 import { Roadmap } from "./assets/components/roadmap";
 import { Showcase } from "./assets/components/showcase";
+import { Form } from "./assets/components/form"
+
 import {Route, Routes} from "react-router-dom"
 
 export function App(){
     return(
         <Routes>
             <Route path="/" element={<>
-                <div className="bg-bandit-bg p-4 md:px-12 md:py-4 lg:px-12 lg:py-4">
+                <div className="bg-bandit-bg p-4 md:px-12 md:py-4 lg:px-50 lg:py-8">
                     <Navbar />
                     <Hero />
                     <Collection />
                     <Showcase />
+                    <Form />
                     <Roadmap />
                     <Contact />
                 </div>
